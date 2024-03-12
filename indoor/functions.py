16f95463eeb48648dd87264a03f102d55a84fd33
Arpit <arpit.mishra.out@gmail.com>
2024-03-12 21:34:18 +0530

    Week0 Lecture of CS50 Introduction to python

---
+++ indoor/functions.py
@@ -0,0 +1,31 @@
+# defining a function
+
+# def hello(msg = 'Nice to meet you!!'):
+#   name = input('Enter your name: ');
+#   print(f"Hello {name}, {msg}");
+
+# hello('I hope you are doing good...');
+# hello();
+
+
+# A Convention to write functions
+# def main():
+#   name = input('What is your name: ');
+#   hello(name);
+
+# def hello(to = "world"):
+#   print("Hello ",to );
+
+# main();
+
+
+
+# def main():
+#   number = int(input('Enter number to be squared: '));
+#   print(f"Square of Number {number}: {square(number)}");
+
+
+# def square(num):
+#   return num*num;
+
+# main();
---
+++ indoor/indoor.py
@@ -0,0 +1,43 @@
+# print('Hello, Python');
+# print(38*25);
+# name = input('What is your name ');
+# name = name.capitalize(); # Use to capitalize the name
+# name = name.title(); # To capitalize each word
+# print('Hello',name,end='');
+# print(' Nice to meet you')
+# year = input('In which year did you born ? ');
+# year = year.strip(); #To remoe the whitespace from the string.
+
+# you can chain multiple methods together
+# print(f'I born in {year} May 20th'); # This is called f-stings in python
+
+# firstName,lastName = name.split(' '); #Split the string into smaller string parts
+# print(firstName,lastName);
+
+# x = input('Enter value 1: ');
+# y = input('Enter value 2: ');
+# z = int(x) + int(y);
+# print(z)
+
+# x = float(input('Enter value 1: '));
+# y = float(input('Enter value 2: '));
+
+# x = round(x,2);
+# print(x)
+# y = round(y,2);# round() leads number to the nearest integer value
+# print(y)
+# print(x+y)
+
+# num1 = float(input('Enter value 1: '));
+# num2 = float(input('Enter value 2: '));
+
+# z = round(num1 + num2);
+# print(f"{z:,}") # format a number
+
+
+# division
+
+# x = 2;
+# y = 3;
+# print(round(x/y,2));
+# print(f"{x/y:.2f}") # To precise a floating point number
